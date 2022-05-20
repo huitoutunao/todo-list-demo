810b0a1aa840b23d9b66eeddb79f6b7a63f33e47
huitoutunao <lh_huitoutunao@163.com>
2022-05-20 16:18:16 +0800

    feat: 新增删除样式和按钮动画

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
         v-model="inputVal"
         type="text"
         placeholder="请输入任务名称"
-        class="input"
+        :class="['input', placeholderStyle ? 'active' : 'inactive']"
         @keyup.enter="onConfirm"
       />
       <button class="confirm" @click="onConfirm">确认</button>
@@ -13,8 +13,10 @@
     <ul class="list-box">
       <li v-for="(item, index) in todoList" :key="index" class="item">
         <div class="check-box">
-          <input :id="item.id" type="checkbox" class="checkbox" />
-          <label :for="item.id" class="label">{{ item.name }}</label>
+          <input :id="item.id" v-model="item.checked" type="checkbox" class="checkbox" />
+          <label :for="item.id" :class="['label', { checked: item.checked }]">{{
+            item.name
+          }}</label>
         </div>
         <button class="del" @click="onDel(index)">删除</button>
       </li>
@@ -23,21 +25,35 @@
 </template>
 
 <script setup lang="ts">
-import { ref, reactive } from 'vue'
+import { ref, reactive, watchEffect } from 'vue'
 
 interface todo {
   id: string
   name: string
+  checked: boolean
 }
 
 const inputVal = ref<string>('')
 const todoList = reactive<todo[]>([])
 const todoIndex = ref<number>(0)
+const placeholderStyle = ref<boolean>(false)
+
+watchEffect(() => {
+  if (inputVal.value) {
+    placeholderStyle.value = false
+  }
+})
 
 const onConfirm = () => {
+  if (!inputVal.value) {
+    placeholderStyle.value = true
+    return
+  }
+
   todoList.push({
     id: `todo${todoIndex.value}`,
     name: inputVal.value,
+    checked: false,
   })
 
   inputVal.value = ''
@@ -80,10 +96,14 @@ ul li {
       color: #333;
       border: none;
       outline: none;
-    }
 
-    input::-webkit-input-placeholder {
-      color: #999;
+      &.inactive::-webkit-input-placeholder {
+        color: #999;
+      }
+
+      &.active::-webkit-input-placeholder {
+        color: #ff0000;
+      }
     }
 
     .confirm {
@@ -110,6 +130,13 @@ ul li {
       padding: 5px 0;
       font-size: 18px;
       color: #333;
+
+      &:hover {
+        .del {
+          opacity: 1;
+          visibility: visible;
+        }
+      }
     }
 
     .check-box {
@@ -127,7 +154,14 @@ ul li {
       .label {
         flex: 1;
         padding-left: 5px;
+        color: #333;
         overflow: hidden;
+        transition: all 0.2s ease;
+
+        &.checked {
+          color: #d8d8d8;
+          text-decoration: line-through;
+        }
       }
     }
 
@@ -137,6 +171,9 @@ ul li {
       color: red;
       background: #fff;
       border: 1px solid red;
+      opacity: 0;
+      visibility: hidden;
+      transition: all 0.2s ease;
       cursor: pointer;
     }
   }
